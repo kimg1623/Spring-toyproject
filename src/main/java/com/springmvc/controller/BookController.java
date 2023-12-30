847d32a4620860ab773f24524abd02f30ae5853d
Jiwon <kimg1623@naver.com>
2023-12-30 11:29:20 +0900

    Feat: 메서드 수준의 @RequestMapping 처리

--- src/main/java/com/springmvc/controller/BookController.java
+++ src/main/java/com/springmvc/controller/BookController.java
@@ -11,17 +11,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 
 @Controller
+@RequestMapping("/books") // 클래스 수준의 @RequestMapping
 public class BookController {
 
     @Autowired // 의존성 주입
     private BookService bookService;
 
-    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    @RequestMapping // 메서드 수준의 @RequestMapping -> method 속성 기본값=GET
     public String requestBookList(Model model){ // 웹 요청 처리할 메서드
         List<Book> list = bookService.getAlliBookList();
         model.addAttribute("bookList", list); // model 객체에 view에 전달할 정보 담는다.
         return "books"; // view
     }
+
+    @RequestMapping("/all")
+    public String requestAllBooks(Model model){ // 웹 요청 처리할 메서드
+        List<Book> list = bookService.getAlliBookList();
+        model.addAttribute("bookList", list); // model 객체에 view에 전달할 정보 담는다.
+        return "books"; // view
+    }
+
+
+
 
 
 }
