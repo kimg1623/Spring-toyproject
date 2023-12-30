a31bbb50ea172157dda6492a7deec0ec59a3c06d
Jiwon <kimg1623@naver.com>
2023-12-30 10:40:26 +0900

    Fix: 애너테이션 추가

--- src/main/java/com/springmvc/repository/BookRepositoryImpl.java
+++ src/main/java/com/springmvc/repository/BookRepositoryImpl.java
@@ -2,10 +2,12 @@ package com.springmvc.repository;
 
 import com.springmvc.domain.Book;
 import com.springmvc.repository.impl.BookRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class BookRepositoryImpl implements BookRepository {
 
     private List<Book> listOfBooks = new ArrayList<>();
